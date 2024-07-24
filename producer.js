210c487594eec8b69cddb713e3badcab59bd303c
rasoolabdi <M0rd00r@yahoo.com>
2024-07-24 11:16:30 +0330

    add pattern for send data with parameters

--- producer.js
+++ producer.js
@@ -1,11 +1,15 @@
 const amqp = require("amqplib");
-
 const ExchangeName = "directMessage";
+
+const [logType , message] = process.argv.slice(2);
+console.log(logType , message);
+
+
 async function sendData() {
     const connection = await amqp.connect("amqp://localhost:5672");
     const channel = await connection.createChannel();
     await channel.assertExchange(ExchangeName , "direct");
-    channel.publish(ExchangeName , "error" , Buffer.from("cannot read data from DB"));   // call routing key error
+    channel.publish(ExchangeName , logType , Buffer.from(message));   // create send data with pattern
     setTimeout(() => {
         connection.close();
         process.exit(0);
