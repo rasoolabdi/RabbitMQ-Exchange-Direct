f4d5966d3c25f122c6fc8ffc559b2fadd213405d
rasoolabdi <M0rd00r@yahoo.com>
2024-07-24 11:29:10 +0330

    add pattern for consumer for get data with pattern

--- consumer.js
+++ consumer.js
@@ -1,12 +1,16 @@
 const amqp = require("amqplib");
 const ExchangeName = "directMessage";
+const logTypes = process.argv.slice(2); //get parameters => error, info , warning
+console.log(logTypes);
 
 async function ReciveData() {
     const connection = await amqp.connect("amqp://localhost:5672");
     const channel = await connection.createChannel();
     await channel.assertExchange(ExchangeName , "direct");
     const assertQueue = await channel.assertQueue("" , {exclusive: true});
-    channel.bindQueue(assertQueue.queue , ExchangeName , "error");
+    for(const pattern of logTypes) {
+        channel.bindQueue(assertQueue.queue , ExchangeName , pattern);
+    }
     channel.consume(assertQueue.queue , (msg) => {
         console.log(msg.content.toString());
     })
