54c11c1cb9b467d8fa829d9e4eff533a5953a053
rasoolabdi <M0rd00r@yahoo.com>
2024-07-24 11:07:00 +0330

    add consumer for recive data

--- consumer.js
+++ consumer.js
@@ -1,1 +1,15 @@
+const amqp = require("amqplib");
+const ExchangeName = "directMessage";
 
+async function ReciveData() {
+    const connection = await amqp.connect("amqp://localhost:5672");
+    const channel = await connection.createChannel();
+    await channel.assertExchange(ExchangeName , "direct");
+    const assertQueue = await channel.assertQueue("" , {exclusive: true});
+    channel.bindQueue(assertQueue.queue , ExchangeName , "error");
+    channel.consume(assertQueue.queue , (msg) => {
+        console.log(msg.content.toString());
+    })
+}
+
+ReciveData();
